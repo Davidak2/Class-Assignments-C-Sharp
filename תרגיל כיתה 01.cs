937670cba0ae9b844c191fe543b437afc90bf615
TheRedSpirit1 <60138656+TheRedSpirit1@users.noreply.github.com>
2020-03-24 12:11:46 +0200

    Add files via upload

---
+++ תרגיל כיתה 01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace מטלת_כיתה
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //מה אורך המערך
+            Console.WriteLine("enter length of array:");
+            int LenghtOfArray = Convert.ToInt32(Console.ReadLine());
+
+            //מה המספר שמכפילים
+            Console.WriteLine("enter number to multiply:");
+            int NumToMult = Convert.ToInt32(Console.ReadLine());
+
+            //הגדרה של אורך המערך
+            int[] arr = new int[LenghtOfArray];
+
+
+            Console.WriteLine($"num of elements:{LenghtOfArray}, num to mult:{NumToMult}");
+
+            //ללולאת הדפסה
+            for(int i=0;  i<arr.Length; i++)
+            {
+                arr[i] = i * NumToMult;
+                Console.WriteLine(arr[i] + "");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
