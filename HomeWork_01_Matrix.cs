63c6ba6743efbea792f6bc6037ba5d8162dbc4f1
Davidak2 <60138656+Davidak2@users.noreply.github.com>
2020-04-02 18:27:22 +0300

    Add files via upload

---
+++ HomeWork_01_Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] matrix1 = new string[3][];//הגדרה של גודל המערך
+
+            matrix1[0] = new string[] { "main", "up", "sub" };
+            matrix1[1] = new string[] { "down", "center", "up" };
+            matrix1[2] = new string[] { "sub", "down", "main" };
+
+            foreach(string[] matrixRow in matrix1)
+            {
+                foreach(string matrixCol in matrixRow)
+                {
+                    Console.Write($"[ {matrixCol} ]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
